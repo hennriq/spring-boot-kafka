5b0ede95fa3959753b7dfbc75c8bcc03e3f56990
Bruno Souza <b.hennriq@hotmail.com>
2021-02-26 12:08:05 -0300

    Inicialização Repositório

---
+++ src/test/java/com/hennriq/Spring/Boot/Kafka/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hennriq.Spring.Boot.Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
